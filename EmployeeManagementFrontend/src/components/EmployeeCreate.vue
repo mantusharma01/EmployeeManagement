575495f3e5ded3cd13ad42067ce15710ced838ba
Mantu Kumar <mantuk@netlink.com>
2023-01-10 12:55:48 +0530

    Employee Registration Page Ui

--- EmployeeManagementFrontend/src/components/EmployeeCreate.vue
+++ EmployeeManagementFrontend/src/components/EmployeeCreate.vue
@@ -1,52 +1,60 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <form @submit="AddEmployee()">
-    <div>
-      <label for="firstName">First Name</label>
-      <input
-        type="text"
-        id="firstName"
-        class="form-control"
-        v-model="firstName"
-      />
-    </div>
-    <div>
-      <label for="lastName">Last Name</label>
-      <input type="text" id="lastName" class="form-control" v-model="lastName" />
-    </div>
-    <div>
-      <label for="email"> Email </label>
-      <input type="text" id="email" class="form-control" v-model="email" />
-    </div>
-    <div>
-      <label for="phoneNumber"> PhoneNumber </label>
-      <input
-        type="text"
-        id="phoneNumber"
-        class="form-control"
-        v-model.number="phoneNumber"
-      
-      />
-    </div>
-    <div>
-      <label for="department">Department</label>
-      <select v-model="selectedDepartment" @change="changeDepartment($event)" class="form-control sl" id="department">
-        <option v-for="list of lists" :key="list.departmentId" :value="list.departmentId">
-          {{ list.departmentName }}
-        </option>
-      </select>
-    </div>
+  <section class="h-100 h-custom" style="background-color: #8fc4b7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-8 col-xl-6">
+        <div class="card rounded-3">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img3.webp"
+            class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .3rem;"
+            alt="Sample photo">
+          <div class="card-body p-4 p-md-5">
+            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registration Info</h3>
+            <form class="px-md-2" @submit="AddEmployee()">
+              <div class="form-outline mb-4">
+                    <input type="text" id="firstName" class="form-control" v-model="firstName"/>
+                    <label class="form-label" for="firstName">Empoloyee First Name</label>
+              </div>
 
-    <div>
-      <label for="location">Location</label>
-      <select v-model="selectedLocation" @change="changeLocation($event)" class="form-control sl" id="location">
-        <option v-for="list of listslocation" v-bind:key="list.loctionId" :value="list.locationId"> 
-          {{ list.locationData }}
-        </option>
-      </select>
+              <div class="form-outline mb-4">
+                    <input type="text" id="lastName" class="form-control" v-model="lastName"/>
+                    <label class="form-label" for="lastName">Empoloyee Last Name</label>
+              </div>
+              <div class="form-outline mb-4">
+                    <input type="text" id="phoneNumber" class="form-control" v-model.number="phoneNumber"/>
+                    <label class="form-label" for="lastName">Empoloyee Phone Number</label>
+              </div>
+              <div class="form-outline mb-4">
+                    <input type="text" id="email" class="form-control" v-model="email" />
+                    <label class="form-label" for="lastName">Empoloyee Email Address</label>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-4">
+                   <select v-model="selectedDepartment" @change="changeDepartment($event)" class="form-control sl" id="department">
+                        <option v-for="list of lists" :key="list.departmentId" :value="list.departmentId">
+                             {{ list.departmentName }}
+                        </option>
+                   </select>
+                   <label for="department">Department</label>
+                </div>
+                <div>      
+                    <select v-model="selectedLocation" @change="changeLocation($event)" class="form-control sl" id="location">
+                         <option v-for="list of listslocation" v-bind:key="list.loctionId" :value="list.locationId"> 
+                              {{ list.locationData }}
+                          </option>
+                    </select>
+                    <label for="location">Location</label>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-success btn-lg mb-1">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-    <button class="btn btn-primary" type="submit" >Submit!</button>
-  </form>
+  </div>
+</section>
 </template>
 <script>
 import axios from "axios";
